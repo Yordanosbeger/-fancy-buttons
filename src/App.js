953830e1459f2c3dd707a5e8e57204396c4aad7f
Yordanos gebreselasie <yordibege@gmail.com>
2023-12-14 02:42:54 -0700

    import components ti app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwithchButton from './components/LightSwitchButton';
 import './App.css';
+import TextRepeaterButton from './components/TextRepeaterButton';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={`App`}>
+      <h1>fancy-Button</h1>
+      <section>
+      <AngryButton/>
+      <CounterButton/>
+      <LightSwithchButton/>
+      <TextRepeaterButton/>
+</section>
     </div>
   );
 }
